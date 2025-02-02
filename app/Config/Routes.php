8193a3e3adc4a42685de1ced2a72a75d2192604f
outnova <bjalastre@gmail.com>
2025-02-01 21:37:11 -0400

    feat: add register - login - auth routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/register', 'AuthController::register');
+$routes->get('/login', 'AuthController::login');
+$routes->post('auth/store', 'AuthController::store');
+$routes->post('auth/authenticate', 'AuthController::authenticate');
+$routes->get('auth/logout', 'AuthController::logout');
