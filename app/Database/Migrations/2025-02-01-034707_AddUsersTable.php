1d56f201e8aeb25702917037eabbb363edd71231
outnova <bjalastre@gmail.com>
2025-02-01 21:34:56 -0400

    feat: add users table migration

---
+++ app/Database/Migrations/2025-02-01-034707_AddUsersTable.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
+
+class AddUsersTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'SERIAL', 
+            ],
+            'cedula' => [
+                'type' => 'BIGINT', 
+                'unique' => true,
+            ],
+            'username' => [
+                'type' => 'VARCHAR', 
+                'constraint' => 255, 
+                'unique' => true,
+            ],
+            'email' => [
+                'type' => 'VARCHAR', 
+                'constraint' => 255, 
+                'unique' => true,
+            ],
+            'password' => [
+                'type' => 'VARCHAR', 
+                'constraint' => 255,
+            ],
+            'active' => [
+                'type' => 'TINYINT', 
+                'constraint' => 1, 
+                'default' => 0,
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP', 
+                'default' => new RawSql('CURRENT_TIMESTAMP'),
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP', 
+                'default' => new RawSql('CURRENT_TIMESTAMP'),
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
+        //
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+        //
+    }
+}
