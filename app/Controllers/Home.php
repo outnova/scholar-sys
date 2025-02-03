834aa2a1fb060bfc04c5941abf658e46003b86f3
outnova <bjalastre@gmail.com>
2025-02-03 17:28:37 -0400

    feat: check if user isloggedin, redirections

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,11 +4,19 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
         //$seeder = \Config\Database::seeder();
         //$seeder->call('UserSeeder');
 
-        return view('auth/login');
+        if (!session()->get('isLoggedIn')) {
+            return redirect()->to('/login')->with('error', 'Debes iniciar sesión.');
+        } 
+
+        if (current_url() === base_url('/')) {
+            return redirect()->to('/home');
+        }
+
+        return view('home');
     }
 }
