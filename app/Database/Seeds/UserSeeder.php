7636d355f4566fe364e76f16eee1eba0a25a23c4
outnova <bjalastre@gmail.com>
2025-02-02 23:12:45 -0400

    feat: create seeder to add admin user

---
+++ app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            'cedula' => '99999999',
+            'username' => 'admin',
+            'email'    => 'example@domain.com',
+            'password' => password_hash('admin', PASSWORD_DEFAULT),
+            'active' => 1,
+        ];
+
+        // Simple Queries
+        $this->db->query('INSERT INTO users (cedula, username, email, password, active) VALUES(:cedula:, :username:, :email:, :password:, :active:)', $data);
+
+        // Using Query Builder
+        $this->db->table('users')->insert($data);
+        //
+    }
+}
