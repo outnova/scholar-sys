139c399126a90e0e90fbe35bd28f073677eecd44
outnova <bjalastre@gmail.com>
2025-02-12 19:21:55 -0400

    refactor: home view improve

--- app/Views/home.php
+++ app/Views/home.php
@@ -4,77 +4,192 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        html, body {
-            height: 100%; /* Asegura que html y body ocupen el 100% de la altura */
-            margin: 0; /* Elimina el margen predeterminado */
-        }
-        body {
-            display: flex;
-            flex-direction: column;
-        }
-        .nav {
-            width: 300px;
-            height: 100%;
-        }
-        .main-container {
-            display: flex;
-            flex-grow: 1; /* Ocupa el espacio restante */
-        }
-        
-    </style>
+    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="<?= base_url('assets/css/sidebar.css'); ?>">
 </head>
 <body>
-    <nav class="navbar shadow-sm bg-primary">
-        <div class="container-fluid">
-            <a class="navbar-brand">
-                <img src="/images/bootstrap-logo.svg" width="36" alt="Logo" />
-            </a>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search..." aria-label="Search">
-                <button class="btn btn-subtle" type="submit"><i class="fa-solid fa-magnifying-glass fa-lg"></i></button>
-            </form>
-            <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                    <a class="avatar" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="/images/avatar/1.jpg" />
-                        <span class="avatar-badge border bg-red-300 p-1"></span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Settings</a></li>
-                        <li><a class="dropdown-item" href="#">Profile</a></li>
-                        <li><a class="dropdown-item" href="#">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
+    <div class="wrapper">
+        <!-- Sidebar -->
+        <aside id="sidebar" class="sidebar">
+            <div class="h-100">
+                <div class="sidebar-logo">
+                    <a href="#">System Name</a>
+                </div>
+                <div class="row g-2 sidebar-info">
+                    <div class="col-auto">
+                        <img src="https://picsum.photos/200" alt="Avatar" class="avatar">
+                    </div>
+                    <div class="col-auto ms-2">
+                        <p class="fw-bold mb-0"><?= session('username'); ?></p>
+                        <p class="mb-0">Cargo</p>
+                    </div>
+                </div>
+                <!-- Sidebar Navigation -->
+                <ul class="sidebar-nav">
+                    <li class="sidebar-header">
+                        Tools & Components
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-solid fa-list pe-2"></i>
+                            Profile
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#pages"
+                            aria-expanded="false" aria-controls="pages">
+                            <i class="fa-regular fa-file-lines pe-2"></i>
+                            Pages
+                        </a>
+                        <ul id="pages" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Analytics</a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Ecommerce</a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Crypto</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard"
+                            aria-expanded="false" aria-controls="dashboard">
+                            <i class="fa-solid fa-sliders pe-2"></i>
+                            Dashboard
+                        </a>
+                        <ul id="dashboard" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Dashboard Analytics</a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Dashboard Ecommerce</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#auth"
+                            aria-expanded="false" aria-controls="auth">
+                            <i class="fa-regular fa-user pe-2"></i>
+                            Auth
+                        </a>
+                        <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Login</a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Register</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-header">
+                        Multi Level Nav
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#multi"
+                            aria-expanded="false" aria-controls="multi">
+                            <i class="fa-solid fa-share-nodes pe-2"></i>
+                            Multi Level
+                        </a>
+                        <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse"
+                                    data-bs-target="#multi-two" aria-expanded="false" aria-controls="multi-two">
+                                    Two Links
+                                </a>
+                                <ul id="multi-two" class="sidebar-dropdown list-unstyled collapse">
+                                    <li class="sidebar-item">
+                                        <a href="#" class="sidebar-link">Link 1</a>
+                                    </li>
+                                    <li class="sidebar-item">
+                                        <a href="#" class="sidebar-link">Link 2</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-solid fa-list pe-2"></i>
+                            Profile
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-solid fa-list pe-2"></i>
+                            Profile
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-solid fa-list pe-2"></i>
+                            Profile
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-solid fa-list pe-2"></i>
+                            Profile
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-solid fa-list pe-2"></i>
+                            Profile
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-solid fa-list pe-2"></i>
+                            Profile
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </aside>
+        <!-- Main Component -->
+        <div class="main">
+            <nav class="navbar navbar-expand px-3 border-bottom d-flex flex-row align-items-center">
+                <!-- Button for sidebar toggle -->
+                <button class="btn" type="button" data-bs-theme="light">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="ms-auto d-flex align-items-center">
+                    <li class="nav-item me-3">
+                        <i class="fa-solid fa-bell fa-lg me-2 text-primary"></i>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="avatar-nav" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="https://picsum.photos/200" class="avatar-nav"/>
+                            <!--<span class="avatar-badge border bg-red-300 p-1"></span>-->
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="#">Settings</a></li>
+                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                            <li><a class="dropdown-item" href="<?= site_url('auth/logout') ?>">Cerrar sesión</a></li> 
+                        </ul>
+                    </li>
+                </div>
+            </nav>
+            <main class="content px-3 py-2 bg-light">
+                <div class="container-fluid">
+                    <div class="mb-3">
+                        <h3>Bootstrap Sidebar Tutorial</h3>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus lectus in nunc bibendum pellentesque. Aliquam tincidunt ligula at nisi pulvinar, ut pharetra ex vulputate. Vivamus non consectetur nisi. Vivamus blandit semper odio sed eleifend. Curabitur vitae blandit ex, a eleifend orci. Donec facilisis egestas aliquet. Suspendisse congue nunc ipsum, at lacinia neque aliquet vel. Aliquam imperdiet aliquam nulla, nec ullamcorper lacus dapibus eget. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur nec quam odio.
+                        </p>
+                    </div>
+                </div>
+            </main>
         </div>
-    </nav>
-    <div class="main-container">
-        <ul class="nav flex-column bg-light shadow p-3 mb-5 bg-body rounded">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Active</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= site_url('auth/logout') ?>">Cerrar sesión</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
-            </li>
-        </ul>
-        
-        <div class="container-fluid p-5">
-            <h3>Lorem itrsum</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis porta metus. Morbi ac ex nec odio lacinia pretium. Cras fringilla eu neque et interdum. Vestibulum egestas enim id lobortis elementum. Pellentesque aliquet purus non arcu bibendum interdum. Duis tempus dui vel nunc semper, ac euismod lorem tincidunt. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis vestibulum orci ligula, ac elementum nisi luctus et.</p>
-            <!-- Content here -->
-        </div>
-      
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="<?= base_url('assets/js/sidebar.js'); ?>"></script>
 </body>
 </html>
