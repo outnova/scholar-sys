b476e56f4b0d24f3727caff151f7445e8fe0bd6a
outnova <bjalastre@gmail.com>
2025-03-21 14:50:04 -0400

    perf: delete old isLoggedIn session check, replaced by auth filter

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,17 +9,6 @@ class Home extends BaseController
         //$seeder = \Config\Database::seeder();
         //$seeder->call('UserSeeder');
 
-        $request = service('request'); // Obtiene la solicitud actual
-
-        if (!session()->get('isLoggedIn')) {
-            // Si la ruta solicitada es exactamente "/home", mostrar el mensaje de error.
-            if ($request->getUri()->getPath() === '/home') {
-                return redirect()->to('/login')->with('error', 'Debes iniciar sesión.');
-            }
-
-            // Si la ruta es "/", solo redirigir sin mensaje de error.
-            return redirect()->to('/login');
-        }
         return view('home');
     }
 }
