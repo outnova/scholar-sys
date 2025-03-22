e42f1309728df995e77d8a1d0195078aa59bc215
outnova <bjalastre@gmail.com>
2025-03-22 00:46:26 -0400

    style: change home welcome message

--- app/Views/home.php
+++ app/Views/home.php
@@ -3,7 +3,7 @@
 <?= $this->section('title') ?>Inicio<?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
-    <h3>Bootstrap Sidebar Tutorial</h3>
+    <h3>Bienvenido al Sistema</h3>
     <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus lectus in nunc bibendum pellentesque. Aliquam tincidunt ligula at nisi pulvinar, ut pharetra ex vulputate. Vivamus non consectetur nisi. Vivamus blandit semper odio sed eleifend. Curabitur vitae blandit ex, a eleifend orci. Donec facilisis egestas aliquet. Suspendisse congue nunc ipsum, at lacinia neque aliquet vel. Aliquam imperdiet aliquam nulla, nec ullamcorper lacus dapibus eget. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur nec quam odio.
     </p>
