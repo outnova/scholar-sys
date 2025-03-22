58f3867aed02c5a541803af22920a015088c237e
outnova <bjalastre@gmail.com>
2025-03-21 20:47:42 -0400

    feat: create admin Users (index) view

---
+++ app/Views/admin/users/index.php
@@ -0,0 +1,10 @@
+<?= $this->extend('templates/base') ?>
+
+<?= $this->section('title') ?>Administrar Usuarios<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+    <h3>Administrar Usuarios</h3>
+    <p>
+    Aquí administrarás los usuarios.
+    </p>
+<?= $this->endSection() ?>
