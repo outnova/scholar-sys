ce62d6e40ef00c5ea8c7dc543fe0e3bdead845f7
outnova <bjalastre@gmail.com>
2025-03-24 12:54:54 -0400

    fix: white space in scroll, set independent v-scroll at content, add "main-content" div, create base.css

--- app/Views/templates/base.php
+++ app/Views/templates/base.php
@@ -6,19 +6,21 @@
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="<?= base_url('assets/css/base.css'); ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/sidebar.css'); ?>">
     <title><?= $this->renderSection('title') ?></title>
 </head>
 <body>
-    <?= $this->include('templates/header'); ?>
-
-            <main class="content px-3 py-2 bg-light">
-                <div class="container-fluid">
-                    <div class="mb-3">
-                        <?= $this->renderSection('content') ?>
+    <div class="main-container">
+        <?= $this->include('templates/header'); ?>
+                <main class="content px-3 py-2 bg-light">
+                    <div class="container-fluid">
+                        <div class="mb-3">
+                            <?= $this->renderSection('content') ?>
+                        </div>
                     </div>
-                </div>
-            </main>
+                </main>
+            </div>
         </div>
     </div>
 
