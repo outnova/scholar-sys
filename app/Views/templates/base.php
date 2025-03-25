3579ccd158a2a6caf521b9c6d572ba0ddedb1570
outnova <bjalastre@gmail.com>
2025-03-24 23:14:15 -0400

    chore-fix: update lineicons cdn to 5.0, add hugecoins cdn, remove bg-light prop from main-container

--- app/Views/templates/base.php
+++ app/Views/templates/base.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
+    <link href="https://cdn.lineicons.com/5.0/lineicons.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.hugeicons.com/font/hgi-stroke-rounded.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="<?= base_url('assets/css/base.css'); ?>">
@@ -18,7 +19,7 @@
 <body>
     <div class="main-container">
         <?= $this->include('templates/header'); ?>
-                <main class="content px-3 py-2 bg-light">
+                <main class="content px-3 py-2">
                     <div class="container-fluid">
                         <div class="mb-3">
                             <?= $this->renderSection('content') ?>
