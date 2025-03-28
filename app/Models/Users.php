d58bc15f8235099779ad6b6cf56ecf4784d43804
outnova <bjalastre@gmail.com>
2025-03-28 00:23:03 -0400

    feat: add position and turn validation rules, add new fields to allowedfields

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -12,8 +12,13 @@ class Users extends Model
     //protected $returnType       = 'array';
     //protected $useSoftDeletes   = false;
     //protected $protectFields    = true;
-    protected $allowedFields    = ['cedula', 'username', 'email', 'password', 'active'];
+    protected $allowedFields    = ['cedula', 'username', 'email', 'password', 'active', 'primer_nombre', 'segundo_nombre', 'primer_apellido', 'segundo_apellido', 'must_change_password', 'id_role', 'position', 'turn'];
 
     // Dates
     protected $useTimestamps = true;
+
+    protected $validationRules = [
+        'position' => 'in_list[Directivo,Subdirectivo,Coordinador,Administrativo]',
+        'turn' => 'in_list[Mañana,Tarde]',
+    ];
 }
