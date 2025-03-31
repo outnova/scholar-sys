858f80434cdc4f8fda5188295d8c25e9d763ffd8
outnova <bjalastre@gmail.com>
2025-03-31 00:22:07 -0400

    refactor: delete show-clean errors functions, moved to individual script file

--- app/Views/admin/users/create.php
+++ app/Views/admin/users/create.php
@@ -103,19 +103,6 @@
         </div>
         <script>
         $(document).ready(function() {
-            // Función para mostrar los errores
-            function showError(input, message) {
-                var errorDiv = $('<div class="error"></div>').text(message);
-                input.closest('.mb-3').append(errorDiv);
-                input.addClass('is-invalid');
-            }
-
-            // Función para limpiar los errores
-            function clearErrors(input) {
-                input.removeClass('is-invalid');
-                input.closest('.mb-3').find('.error').remove();
-            }
-
             // Verificación de cédula (debe ser entre 6 y 9 dígitos)
             $('#cedula').on('input', function() {
                 var cedula = $(this).val();
