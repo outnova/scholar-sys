f264f249c1fecf2287fa6060d89ae819842530a9
outnova <bjalastre@gmail.com>
2025-04-04 01:44:53 -0400

    feat-style: add 'estado' column to users table, add actions icons, set responsive the table, text-align to center

--- app/Views/admin/users/index.php
+++ app/Views/admin/users/index.php
@@ -8,28 +8,43 @@
 
             <a href="<?= base_url('/admin/users/create') ?>" class="btn btn-success mb-3">Agregar usuario</a>
 
-            <table id="users" class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Usuario</th>
-                        <th>Correo</th>
-                        <th>Cédula</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($users as $user): ?>
-                    <tr>
-                        <td><?= esc($user['id']) ?></td>
-                        <td><?= esc($user['username']) ?></td>
-                        <td><?= esc($user['email']) ?></td>
-                        <td><?= esc($user['cedula']) ?></td>
-                        <td>Editar / Eliminar</td>
-                    </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+            <div class="table-responsive-md">
+                <table id="users" class="table table-striped table-bordered w-100" style="text-align: center;">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Usuario</th>
+                            <th>Correo</th>
+                            <th>Cédula</th>
+                            <th>Estado</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($users as $user): ?>
+                        <tr>
+                            <td><?= esc($user['id']) ?></td>
+                            <td><?= esc($user['username']) ?></td>
+                            <td><?= esc($user['email']) ?></td>
+                            <td><?= esc($user['cedula']) ?></td>
+                            <td>
+                                <span class="badge <?= $user['active'] ? 'bg-success' : 'bg-danger' ?>"><?= $user['active'] ? 'Activo' : 'Inactivo' ?></span>
+                            </td>
+                            <td>
+                            <div class="item-action d-flex gap-3 fs-5 justify-content-center">
+                                <a href="#" title="Ver usuario">
+                                    <i class="hgi hgi-stroke hgi-view"></i>
+                                </a>
+                                <a href="#" title="Editar usuario">
+                                    <i class="hgi hgi-stroke hgi-pencil-edit-02"></i>
+                                </a>
+                            </div>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
 
         <script>
