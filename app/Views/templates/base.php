16776d39cb6b665bacad265ea284f66748e648a0
outnova <bjalastre@gmail.com>
2025-04-04 13:50:25 -0400

    fix: add styles-scripts sections in base template

--- app/Views/templates/base.php
+++ app/Views/templates/base.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="<?= base_url('assets/css/base.css'); ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/sidebar.css'); ?>">
+    <!-- Sección para estilos personalizados -->
+    <?= $this->renderSection('styles') ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
@@ -75,5 +77,7 @@
 
     <script src="<?= base_url('assets/js/sidebar.js'); ?>"></script>
     <script src="<?= base_url('assets/js/errors.js'); ?>"></script>
+    <!-- Sección para scripts personalizados -->
+    <?= $this->renderSection('scripts') ?>
 </body>
 </html>
