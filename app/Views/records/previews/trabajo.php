1dd27a460534bcc0c4d889894e891fb6370c58eb
outnova <bjalastre@gmail.com>
2025-06-02 15:41:10 -0400

    feat: create 'trabajo' record type content

---
+++ app/Views/records/previews/trabajo.php
@@ -0,0 +1,15 @@
+<p class="paragraph">
+    Quien suscribe <b><i><?= esc(get_setting('principal_name')) ?></i></b>, portador de la cédula de identidad Nº <b><i><?= esc(get_setting('principal_ci')) ?></i></b>, en mi condición de Director (e) de la <b><i><?= esc(get_setting('school_name')) ?></i></b>, ubicada en <b><i><?= esc(get_setting('school_address')) ?></i></b>.
+</p>
+<p class="paragraph">
+    Por medio de la presente, hago constar que el (la) ciudadano(a): <b><i><?= esc($data['primer_nombre']) ?> <?= esc($data['segundo_nombre']) ?> <?= esc($data['primer_apellido']) ?> <?= esc($data['segundo_apellido']) ?></i></b>, 
+    portador(a) de la cédula de identidad N° <b><i><?= esc($data['cedula']) ?></i></b> cumple funciones en esta institución educativa como: <b><i><?= esc($data['cargo_funciones']) ?></i></b>, código de cargo: <b><i><?= esc($data['codigo_cargo']) ?></i></b>,
+    dependencia: <b><i><?= esc($data['dependencia']) ?></i></b>, código de dependencia: <b><i><?= esc($data['codigo_dependencia']) ?></i></b>, en el nivel de: <b><i><?= esc($data['nivel']) ?></i></b> en nuestra institución desde el <b><i><?= esc($data['fecha_ingreso']) ?></i></b>
+    hasta la presente fecha; devengando una remuneración mensual de Bs. <b><i><?= esc($data['sueldo_mensual']) ?></i></b> de acuerdo al recibdo de pago emanado por la Oficina de Gestión Humana del Ministerio del Poder Popular para la Educación.
+</p>
+<p class="paragraph">
+    Constancia que se emite en Santa Ana de Coro; a los 13 días.
+</p>
+<p class="paragraph">
+    Sin más a que referir, se despide;
+</p>
