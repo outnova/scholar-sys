1854c3968ece4376682093021015bcbdedd91f51
outnova <bjalastre@gmail.com>
2025-06-02 21:49:11 -0400

    style: add <u> tag in pdf record content ('trabajo' type)

--- app/Views/records/previews/trabajo.php
+++ app/Views/records/previews/trabajo.php
@@ -1,11 +1,11 @@
 <p class="paragraph">
-    Quien suscribe <b><i><?= esc(get_setting('principal_name')) ?></i></b>, portador de la cédula de identidad Nº <b><i><?= esc(get_setting('principal_ci')) ?></i></b>, en mi condición de Director (e) de la <b><i><?= esc(get_setting('school_name')) ?></i></b>, ubicada en <b><i><?= esc(get_setting('school_address')) ?></i></b>.
+    Quien suscribe <b><i><u><?= esc(get_setting('principal_name')) ?></u></i></b>, portador de la cédula de identidad Nº <b><i><u><?= esc(get_setting('principal_ci')) ?></u></i></b>, en mi condición de Director (e) de la <b><i><u><?= esc(get_setting('school_name')) ?></u></i></b>, ubicada en <b><i><u><?= esc(get_setting('school_address')) ?></u></i></b>.
 </p>
 <p class="paragraph">
-    Por medio de la presente, hago constar que el (la) ciudadano(a): <b><i><?= esc($data['primer_nombre']) ?> <?= esc($data['segundo_nombre']) ?> <?= esc($data['primer_apellido']) ?> <?= esc($data['segundo_apellido']) ?></i></b>, 
-    portador(a) de la cédula de identidad N° <b><i><?= esc($data['cedula']) ?></i></b> cumple funciones en esta institución educativa como: <b><i><?= esc($data['cargo_funciones']) ?></i></b>, código de cargo: <b><i><?= esc($data['codigo_cargo']) ?></i></b>,
-    dependencia: <b><i><?= esc($data['dependencia']) ?></i></b>, código de dependencia: <b><i><?= esc($data['codigo_dependencia']) ?></i></b>, en el nivel de: <b><i><?= esc($data['nivel']) ?></i></b> en nuestra institución desde el <b><i><?= esc($data['fecha_ingreso']) ?></i></b>
-    hasta la presente fecha; devengando una remuneración mensual de Bs. <b><i><?= esc($data['sueldo_mensual']) ?></i></b> de acuerdo al recibdo de pago emanado por la Oficina de Gestión Humana del Ministerio del Poder Popular para la Educación.
+    Por medio de la presente, hago constar que el (la) ciudadano(a): <b><i><u><?= esc($data['primer_nombre']) ?> <?= esc($data['segundo_nombre']) ?> <?= esc($data['primer_apellido']) ?> <?= esc($data['segundo_apellido']) ?></u></i></b>, 
+    portador(a) de la cédula de identidad N° <b><i><u><?= esc($data['cedula']) ?></u></i></b> cumple funciones en esta institución educativa como: <b><i><u><?= esc($data['cargo_funciones']) ?></u></i></b>, código de cargo: <b><i><u><?= esc($data['codigo_cargo']) ?></u></i></b>,
+    dependencia: <b><i><u><?= esc($data['dependencia']) ?></u></i></b>, código de dependencia: <b><i><u><?= esc($data['codigo_dependencia']) ?></u></i></b>, en el nivel de: <b><i><u><?= esc($data['nivel']) ?></u></i></b> en nuestra institución desde el <b><i><u><?= esc($data['fecha_ingreso']) ?></u></i></b>
+    hasta la presente fecha; devengando una remuneración mensual de <b><i><u>Bs. <?= esc($data['sueldo_mensual']) ?></u></i></b> de acuerdo al recibdo de pago emanado por la Oficina de Gestión Humana del Ministerio del Poder Popular para la Educación.
 </p>
 <p class="paragraph">
     Constancia que se emite en Santa Ana de Coro; a los 13 días.
