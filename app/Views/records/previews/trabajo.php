826a52241fd76225ec57ebbeb28cf2b625f34972
outnova <bjalastre@gmail.com>
2025-06-05 22:04:15 -0400

    fix-style: "Bs ..." use "money" class instead of "record-data" to show sueldo_mensual

--- app/Views/records/previews/trabajo.php
+++ app/Views/records/previews/trabajo.php
@@ -5,7 +5,7 @@
     Por medio de la presente, hago constar que el (la) ciudadano(a): <span class="record-data"><?= esc($data['primer_nombre']) ?> <?= esc($data['segundo_nombre']) ?> <?= esc($data['primer_apellido']) ?> <?= esc($data['segundo_apellido']) ?></span>, 
     portador(a) de la cédula de identidad N° <span class="record-data"><?= formatCedula($data['cedula']) ?></span> cumple funciones en esta institución educativa como: <span class="record-data"><?= esc($data['cargo_funciones']) ?></span>, código de cargo: <span class="record-data"><?= esc($data['codigo_cargo']) ?></span>,
     dependencia: <span class="record-data"><?= esc($data['dependencia']) ?></span>, código de dependencia: <span class="record-data"><?= esc($data['codigo_dependencia']) ?></span>, en el nivel de: <span class="record-data">educación <?= esc($data['nivel']) ?></span> en nuestra institución desde el <span class="record-data"><?= formatDate($data['fecha_ingreso']) ?></span>
-    hasta la presente fecha; devengando una remuneración mensual de <span class="money">Bs.</span> <span class="record-data"><?= esc($data['sueldo_mensual']) ?></span> de acuerdo al recibo de pago emanado por la Oficina de Gestión Humana del Ministerio del Poder Popular para la Educación.
+    hasta la presente fecha; devengando una remuneración mensual de <span class="money">Bs. <?= esc($data['sueldo_mensual']) ?></span> de acuerdo al recibo de pago emanado por la Oficina de Gestión Humana del Ministerio del Poder Popular para la Educación.
 </p>
 <p class="paragraph">
     Constancia que se emite en <?= esc(get_setting('school_shortcity')) ?> a los <?= literalRecordDate(true) ?>.
