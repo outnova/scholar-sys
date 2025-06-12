02a34ff8254dc096a1a98fc57bd80be6623fe62a
outnova <bjalastre@gmail.com>
2025-06-11 22:55:04 -0400

    feat: add valid_periodo custom rule

--- app/Libraries/Validation/customRules.php
+++ app/Libraries/Validation/customRules.php
@@ -13,4 +13,11 @@ class customRules
         
         return $fecha <= strtotime(date('Y-m-d'));
     }
+    public function valid_periodo(string $str, ?string &$fields = null, ?array &$data = null): bool
+    {
+        if (preg_match('/^(20\d{2})-(20\d{2})$/', $str, $match)) {
+            return ((int)$match[2] === (int)$match[1] + 1);
+        }
+        return false;
+    }
 }
