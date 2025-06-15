e9a97333e81e9cf92350075600bd8b03534767d5
outnova <bjalastre@gmail.com>
2025-06-14 22:51:49 -0400

    feat: create after_or_equal_fecha_inicio custom rule

--- app/Libraries/Validation/customRules.php
+++ app/Libraries/Validation/customRules.php
@@ -20,4 +20,18 @@ class customRules
         }
         return false;
     }
+    public function after_or_equal_fecha_inicio(string $str, ?string &$fields = null, ?array &$data = null): bool
+    {
+        $fechaInicio = $data[$fields] ?? null;
+        $fechaFin = $str;
+        
+        if (empty($fechaInicio) || empty($fechaFin)) {
+            return false;
+        }
+
+        $inicio = strtotime($fechaInicio);
+        $fin = strtotime($fechaFin);
+
+        return $fin > $inicio;
+    }
 }
